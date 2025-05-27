40d5b1c7a36b707c787aca1bf690f64a5de8c991
tayebeheghbalzadeh <tayebeh.eghbalzadeh@gmail.com>
2025-05-27 17:11:56 +0330

    Declare the players score variables

--- javaScript.js
+++ javaScript.js
@@ -1,9 +1,10 @@
-/* ---------------------------------------------------------------
+/* 
     UNDERESTAND the problem:
     a function that choose a random word from "rock" , "paper" , "scissors"
     input : we don't have
     output : random choice from "rock" , "paper" , "scissors"
--------------------------------------------------------------------
+
+
     PLAN && PSEUDOCODE:
     one) we have three choices :"rock" , "paper" , "scissors"
     two) choose a random number betweeb 0 and 1 using math.random()
@@ -12,6 +13,7 @@
         if 0.34 =< number < 0.67 => "paper"
         if number => 0.67 = "scissors"
     four)return the choice
+  
 */
 // Computer Choice step 1
 function getComputerChoice(){
@@ -31,20 +33,21 @@ console.log(getComputerChoice());
 console.log(getComputerChoice());
 console.log(getComputerChoice());
 
-/*------------
+////////////////////////////////////////////////////
+
+/*
 undrestand:
 a funtion that get the choice of the user and return it
 input : "rock" , "paper" , "scissors"
 output : "rock" , "paper" , "scissors"
 
-------------
+
 plan && pseudocode :
 create a function
 prompt the user choice
 assign the prompt to a varrible
 return the varible
 
-------------
 */
 //human choice  step two
 function getHumanChoice(){
@@ -52,3 +55,8 @@ function getHumanChoice(){
     return userInput;
 }
 console.log(getHumanChoice());
+////////////////////////////////////////////////////s
+
+//  step three
+let humanScore = 0;
+let computerScore = 0;
